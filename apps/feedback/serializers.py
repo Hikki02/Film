03b1803b1fab5f6954545444cd3a957aadcef073
Hikki02 <a.aidaraliev25@gmail.com>
2022-05-29 02:43:35 +0500

    add feedback

---
+++ apps/feedback/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import FeedBack
+
+
+class FeedbackSerializers(serializers.ModelSerializer):
+    class Meta:
+        model = FeedBack
+        fields = ('name', 'email', 'type_message', 'text', )
--- apps/feedback/views.py
+++ apps/feedback/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
 
-# Create your views here.
+from .serializers import FeedbackSerializers
+
+
+class CreateFeedback(APIView):
+    def post(self, request):
+        serializer = FeedbackSerializers(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
